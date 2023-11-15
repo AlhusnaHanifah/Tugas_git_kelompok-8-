912bdfa1a2652521500d1f414e169ecae9ba84cc
WidyaNurulSukma <131254562+WidyaNurulSukma@users.noreply.github.com>
2023-11-15 10:33:53 +0700

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,3 +1,18 @@
+/**
+* Class SavingsAccount adalah turunan dari kelas Account dan merepresentasikan jenis akun tabungan.
+* Konstruktor SavingsAccount digunakan untuk inisialisasi objek dengan informasi dasar, seperti nomor akun,
+* saldo awal, pemilik akun, dan tingkat bunga. Konstruktor ini memanggil konstruktor kelas induk (Account)
+* menggunakan kata kunci 'super'.
+
+* Metode withdraw di-override untuk menangani penarikan uang dari akun tabungan. Jika saldo mencukupi, 
+* penarikan berhasil dan saldo dikurangi sesuai jumlah penarikan. Jika tidak, pesan kesalahan ditampilkan.
+
+* Kode program ini memanfaatkan konsep inheritance dalam pemrograman berorientasi objek,
+* di mana SavingsAccount mewarisi atribut dan metode dari kelas induknya, Account.
+
+* @author   : Widya Nurul Sukma
+* @version  : 14-11-2023
+*/
 
 public class SavingsAccount extends Account {
     private double interestRate;
